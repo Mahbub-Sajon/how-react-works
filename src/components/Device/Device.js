f5166b47eee3c27f2ffc9b6760d25823d2daa9a7
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-03-29 12:02:54 +0600

    first commit

---
+++ src/components/Device/Device.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Device = (props) => {
+    return (
+        <div>
+            <h2>The Price: {props.price}</h2>
+        </div>
+    );
+};
+
+export default Device;
